ea2169f2c01e11cc23dce37d8a39c27026f29ec2
wzzll123 <49931300+wzzll123@users.noreply.github.com>
2020-05-04 10:55:04 +0800

    Add files via upload
    添加了获得问题列表和添加问题的接口

---
+++ youknowapp_problem/OpenHelper.java
@@ -0,0 +1,41 @@
+package com.example.youkonwapp;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class OpenHelper extends SQLiteOpenHelper {
+
+    //建表语句
+    public static final String CREATE_USER = "create table User ("
+            + "id integer primary key autoincrement, "
+            + "username text, "
+            + "userpwd text)";
+    public static final String CREATE_Problem = "create table Problem ("
+            + "Problemid integer primary key autoincrement, "
+            + "username text,"
+            + "problem text, "
+            + "description text)";
+
+    public OpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                      int version) {
+        super(context, name, factory, version);
+
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+
+        db.execSQL(CREATE_USER);//创建用户表
+        db.execSQL(CREATE_Problem);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ youknowapp_problem/ProblemActivity.java
@@ -0,0 +1,13 @@
+package com.example.youkonwapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProblemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
